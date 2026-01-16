fa4c0b8661e06bcb19f65bd56abd099834e046a3
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-01-16 06:22:22 +0000

    finish help command

--- src/routines/help.rs
+++ src/routines/help.rs
@@ -2,8 +2,10 @@ pub fn help() {
     print!(
         "===== COMMANDS =====,
 
-help: Show this screen,
-fetch-market-data: Download the latest market data for analysis.
+help:               Show this screen,
+fetch-market-data:  Download the latest market data for analysis, saving the results in /data.
+build-surface:      Build the volatility surface by analysing the downloaded data, saving the results in /data.
+build-graphs:       Create graphs showing the implied volatility against strike price for each option expiry, saving the results in /data/graphs.
 "
     )
 }
