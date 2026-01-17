edf1e67151bab511937eb1efdc1a51189c3fc897
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-01-17 03:27:01 +0000

    update readme and console formatting

--- src/routines/fetch_market_data.rs
+++ src/routines/fetch_market_data.rs
@@ -7,11 +7,11 @@ use crate::integrations::DeribitTickerData;
 use crate::network;
 
 pub async fn fetch_market_data() {
-    println!("===========================================================");
-    println!("===========================================================");
+    println!("===============================================================");
+    println!("===============================================================");
     println!("Fetching Bitcoin market data and saving to file");
-    println!("===========================================================");
-    println!("===========================================================");
+    println!("===============================================================");
+    println!("===============================================================");
 
     println!("Fetching options...");
     let mut options = network::do_rpc_request_as_struct::<Vec<DeribitOptionInstrument>>(
@@ -59,5 +59,5 @@ pub async fn fetch_market_data() {
     };
 
     fileio::save_struct_to_file(&data, "./data/deribit-btc-market-data.json");
-    println!("===========================================================");
+    println!("===============================================================");
 }
