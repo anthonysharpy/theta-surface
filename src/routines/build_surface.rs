7e5dfd7c1952dc7292e285bf444760237d209048
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-01-22 07:01:18 +0000

    improve smile fitting output

--- src/routines/build_surface.rs
+++ src/routines/build_surface.rs
@@ -129,7 +129,8 @@ fn fit_smile_graphs(smile_graphs: &mut Vec<SmileGraph>) {
 
     for graph in smile_graphs.iter_mut() {
         let current_smile = succeeded_smiles + failed_smiles + 1;
-        println!("Fitting smile {current_smile}...");
+        println!("");
+        println!("Fitting smile {current_smile} ({})...", graph.options[0].get_expiration().to_rfc3339());
 
         match graph.fit_smile() {
             Err(e) => {
