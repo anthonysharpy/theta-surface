325263e2de543cc89b76e9474e6bbe7c1f870d7e
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-01-23 00:13:47 +0000

    normalise all spot prices to be the same

--- src/routines/fetch_market_data.rs
+++ src/routines/fetch_market_data.rs
@@ -12,19 +12,23 @@ pub async fn fetch_market_data() {
     println!("===============================================================");
     println!("===============================================================");
 
+    let mut options = download_options().await;
+    println!("------------------------------");
+
+    normalise_data(&mut options);
+    println!("------------------------------");
+
+    save_data(options);
+    println!("===============================================================");
+}
+
+async fn download_options() -> Vec<DeribitOptionInstrument> {
     println!("Fetching options...");
     let mut options = network::do_rpc_request_as_struct::<Vec<DeribitOptionInstrument>>(
         "https://www.deribit.com/api/v2/public/get_instruments?currency=BTC&kind=option&expired=false",
     )
     .await
     .expect("Failed downloading options");
-
-    println!("Fetching index price...");
-    let index_price = network::do_rpc_request_as_struct::<DeribitIndexPrice>(
-        "https://www.deribit.com/api/v2/public/get_index_price?index_name=btc_usd",
-    )
-    .await
-    .expect("Failed downloading index price");
 
     let mut i: usize = 0;
 
@@ -54,13 +58,25 @@ pub async fn fetch_market_data() {
         i += 1;
     }
 
+    options
+}
+
+/// The data has some anomalies because we can't download it all in one go. For example, the spot prices will be different
+/// for no reason. We can improve the quality of the data by adjusting that.
+fn normalise_data(options: &mut Vec<DeribitOptionInstrument>) {
+    println!("Normalising data...");
+
+    let spot_price = options[0].ticker_data.as_ref().unwrap().index_price;
+
+    for option in options {
+        option.ticker_data.as_mut().unwrap().index_price = spot_price;
+    }
+}
+
+fn save_data(options: Vec<DeribitOptionInstrument>) {
     println!("Saving data to file...");
 
-    let data = DeribitDataContainer {
-        options: options,
-        index_price: index_price,
-    };
+    let data = DeribitDataContainer { options: options };
 
     fileio::save_struct_to_file(&data, "./data/deribit-btc-market-data.json");
-    println!("===============================================================");
 }
