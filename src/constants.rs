d2f4a55dba70e7082fa2472f87eac6178e61ac35
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-01-24 14:55:11 +0000

    remove unused constant

--- src/constants.rs
+++ src/constants.rs
@@ -1,9 +1,5 @@
 // Some constants based on assumptions. These could be refactored into program parameters.
 
-/// The loss given to an invalid SVI curve. For reference, we typically get total loss of < 1 in the final result, so
-/// this is a very high amount.
-pub const INVALID_SVI_LOSS: f64 = 999.0;
-
 /// The assumed interest free rate used when calculating the forward price. In reality we would figure this out by
 /// doing thinks like looking at the market (e.g. from futures pricing), but that's too much work. Having looked at
 /// the futures data, it seems this is typically implied to be around 5-8%, depending on expiry. So we'll use a sensible
