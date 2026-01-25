f00bac198743443510ea245a3d710ee8108decee
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-01-25 08:09:28 +0000

    revert bounds checks changes

--- src/analytics/types.rs
+++ src/analytics/types.rs
@@ -1,5 +1,3 @@
-use nalgebra::Vector5;
-
 use crate::{analytics::SmileGraph, constants, types::UnsolveableError};
 
 // The parameters that define the SVI smile curve function
@@ -17,7 +15,7 @@ impl SVICurveParameters {
     pub fn new_empty() -> SVICurveParameters {
         let params: SVICurveParameters = SVICurveParameters {
             a: 0.0001,
-            b: 0.0001,
+            b: 0.0002,
             p: 0.0001,
             m: 0.0001,
             o: 0.0001,
@@ -42,10 +40,6 @@ impl SVICurveParameters {
         Ok(params)
     }
 
-    pub fn to_vector(&self) -> Vector5<f64> {
-        Vector5::new(self.a, self.b, self.p, self.m, self.o)
-    }
-
     pub fn get_a(&self) -> f64 {
         self.a
     }
@@ -68,21 +62,21 @@ impl SVICurveParameters {
 
     /// Assert that the maths is correct.
     pub fn check_valid(params: &SVICurveParameters) -> Result<(), UnsolveableError> {
-        if params.b < 0.0001 {
+        if params.b < 0.0 {
             return Err(UnsolveableError::new(format!("b cannot be less than zero ({})", params.b)));
         }
-        if params.p < -0.9999 {
+        if params.p < -1.0 {
             return Err(UnsolveableError::new("p must be greater than -1"));
         }
-        if params.p > 0.9999 {
+        if params.p >= 1.0 {
             return Err(UnsolveableError::new("p must be smaller than 1"));
         }
-        if params.o < 0.0001 {
+        if params.o <= 0.0 {
             return Err(UnsolveableError::new("o must be greater than 0"));
         }
 
         // Assert non-negative variance.
-        if params.a + (params.b * params.o * (1.0 - params.p.powf(2.0)).sqrt()) < 0.0001 {
+        if params.a + (params.b * params.o * (1.0 - params.p.powf(2.0)).sqrt()) <= 0.0 {
             return Err(UnsolveableError::new("Variance must be greater than 0"));
         }
 
@@ -93,17 +87,17 @@ impl SVICurveParameters {
         }
 
         // Assert Lee's moment formula consistent.
-        if params.b * (1.0 + params.p) < 0.0001 {
-            return Err(UnsolveableError::new("Asymptotic slope of total variance must be greater than 0"));
+        if params.b * (1.0 + params.p) <= 0.0 {
+            return Err(UnsolveableError::new("Asymptotic slope of total variance must be greater than 0 (a)"));
         }
-        if params.b * (1.0 + params.p) > 1.9999 {
-            return Err(UnsolveableError::new("Asymptotic slope of total variance must be less than 2"));
+        if params.b * (1.0 + params.p) >= 2.0 {
+            return Err(UnsolveableError::new("Asymptotic slope of total variance must be less than 2 (a)"));
         }
-        if params.b * (1.0 - params.p) < 0.0001 {
-            return Err(UnsolveableError::new("Asymptotic slope of total variance must be greater than 0"));
+        if params.b * (1.0 - params.p) <= 0.0 {
+            return Err(UnsolveableError::new("Asymptotic slope of total variance must be greater than 0 (b)"));
         }
-        if params.b * (1.0 - params.p) > 1.9999 {
-            return Err(UnsolveableError::new("Asymptotic slope of total variance must be less than 2"));
+        if params.b * (1.0 - params.p) >= 2.0 {
+            return Err(UnsolveableError::new("Asymptotic slope of total variance must be less than 2 (b)"));
         }
 
         Ok(())
