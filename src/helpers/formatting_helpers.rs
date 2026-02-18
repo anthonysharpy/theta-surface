93d096684cb8be5305f0a0883e4a9e5534805296
Anthony Sharp <anthony.sharp@payment-assist.co.uk>
2026-02-18 05:11:09 +0000

    minor code quality changes

--- src/helpers/formatting_helpers.rs
+++ src/helpers/formatting_helpers.rs
@@ -1,13 +1,11 @@
-use std::f64;
-
 pub trait F64Helpers {
-    /// NOTE: probably doesn't support the full range of f64.
-    fn round_to_decimal_places(&self, places: i32) -> f64;
+    /// NOTE: will not work for very large floats or number of places.
+    fn round_to_decimal_places(self, places: u16) -> f64;
 }
 
 impl F64Helpers for f64 {
-    fn round_to_decimal_places(&self, places: i32) -> f64 {
-        let scalar = 10_f64.powi(places);
-        ((self * scalar).round() as i64) as f64 / scalar
+    fn round_to_decimal_places(self, places: u16) -> f64 {
+        let scalar = 10_f64.powi(places as i32);
+        (self * scalar).round() / scalar
     }
 }
